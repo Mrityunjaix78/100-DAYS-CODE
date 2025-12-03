8c97ae550425b03d7ccc5a6d83b9b7e9b27384f1
Mrityunjai x 78 <aryansingh15082007@gmail.com>
2025-12-03 13:09:29 +0530

    Update q141.c

---
+++ q141.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    int marks;
+};
+
+void main() {
+    struct Student s;
+
+    printf("Enter Name: ");
+    scanf("%s", s.name);
+
+    printf("Enter Roll No: ");
+    scanf("%d", &s.roll_no);
+
+    printf("Enter Marks: ");
+    scanf("%d", &s.marks);
+
+    printf("Name: %s | Roll: %d | Marks: %d\n", s.name, s.roll_no, s.marks);
+}
